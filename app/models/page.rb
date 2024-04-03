f0c7e6b20284c46f961edfb2713278fdc89f27d9
Mohd Khairi Mohd Adnan <khairi@labs.my>
2024-04-03 23:08:37 +0800

    Update page view

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ApplicationRecord
+  # Associations
+
+  # Validations
+  validates :title, presence: true, uniqueness: true, length: { minimum: 5, maximum: 150 }
+  validates :content, presence: true
+
+  # Callbacks
 end
