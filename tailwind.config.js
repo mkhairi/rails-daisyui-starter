c93ba6d10cbbc1d78108370ee778049d46cd50f8
Mohd Khairi Mohd Adnan <khairi@labs.my>
2025-05-16 19:12:46 +0800

    Update source and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,5 @@
 export default {
-  mode: 'jit',
-  content: [
-    './app/views/**/*.{slim,erb,jbuilder,turbo_stream,js}',
-    './app/decorators/**/*.rb',
-    './app/helpers/**/*.rb',
-    './app/inputs/**/*.rb',
-    './app/assets/javascripts/**/*.js',
-    './config/initializers/**/*.rb',
-    './lib/components/**/*.rb'
-  ],
-  safelist: [
-    'badge-*',
-  ],
-  variants: {
-    extend: {
-      overflow: ['hover']
-    }
-  },
-  theme: {
-    listStyleType: {
-      none: 'none',
-      disc: 'disc',
-      decimal: 'decimal',
-      square: 'square'
-    }
-  },
-  plugins: {
-    '@tailwindcss/postcss': {},
-  }
+  content: [],
+  safelist: [],
+  plugins: []
 }
